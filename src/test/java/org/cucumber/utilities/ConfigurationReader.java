eb6d6a373dd6b4bd62dfb13d692ae02b8b95be87
Suleyman Sarikurk <suleymansarikurk@gmail.com>
2025-11-30 11:37:51 +0300

    preparation2

---
+++ src/test/java/org/cucumber/utilities/ConfigurationReader.java
@@ -0,0 +1,34 @@
+package org.cucumber.utilities;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+/**
+ * reads the properties file configuration.properties
+ */
+public class ConfigurationReader {
+
+    private static Properties properties;
+
+    static {
+
+        try {
+            String path = "configuration.properties";
+//            String path = "/Users/ss/Documents/SampleJava/configuration.properties";
+//            /Users/ss/Documents/SampleJava/configuration.properties
+            FileInputStream input = new FileInputStream(path);
+            properties = new Properties();
+            properties.load(input);
+
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
+    public static String get(String keyName) {
+        return properties.getProperty(keyName);
+    }
+
+}
